f47d252065c3a807ac4781359b9859b83e4f3749
astmetale <astmetale@gmail.com>
2026-02-16 17:55:44 -0600

    primer commit

---
+++ JavaScript/1 Introduccion/javaS1.js
@@ -0,0 +1,22 @@
+string = "cadena de texto"
+number = 19
+booleano = false
+const constante = "alejandro"; //Valor const no se puede modificar
+// Const se tiene que inicializar cuando se declare
+let numero; 
+alert(numero);
+// Si no se crea su valor en el alert es undefined ya que no tiene valor
+numero = 29;
+alert(numero); //Ahora si tiene un valor y en vez de undefined su valor es 29
+
+let numero1 = 23, numero2 = 39;// Tambien se pueden poner dos variables juntas asi <-
+
+let numeroNull = null; //Es una variable que se crea vacia a proposito cuenta como inicializaada
+
+let numero3 = 1;
+let numero4 = 2;
+alert(numero3+numero4); //Aqui si suma los dos numeros porque tienen un valor
+
+let numberNan1;
+let numberNan2;
+alert(numberNan1 * numberNan2); //Aqui no, aqui de NaN (Not a number) porque no están dadas de alta
