9bd736d3a9b1292c1a30d01df68622229efbbf8b
astmetale <astmetale@gmail.com>
2026-02-20 00:00:21 -0600

    for in and of

--- JavaScript/Capitulo 2/javaScript.js
+++ JavaScript/Capitulo 2/javaScript.js
@@ -3,10 +3,26 @@ document.writeln(fruta[0]+"<br>");
 
 numb = 1;
 while(numb <= 5){
-    document.writeln(numb+"<br>");
+    document.writeln(numb+" esto es un while <br>");
     numb++;
 }
 
 for(let i=1; i<11; i++){
-    document.writeln("El numero es: "+i+"<br>")
+    if(i == 6){
+        continue; //Indica que en esta iteracion no haga nada y pase a la siguiente iteracion
+
+    }
+    document.writeln("El numero es: "+i+" esto es un for <br>");
+
+}
+
+let animales = ['gato', 'perro','pato'];
+
+for(animal in animales){
+    document.writeln(animal+'<br>'); //for in nos dice la posicion de lo que esta escrito en este caso 0,1,2
+}
+document.writeln('<br>');
+
+for(animal of animales){
+    document.writeln(animal);//for of en vez de decir la posicion nos dice cada uno de los animales en orden
 }
