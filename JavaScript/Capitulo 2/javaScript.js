21b7ec10718f15a34091063126740668c71fdb4b
astmetale <astmetale@gmail.com>
2026-02-19 23:07:20 -0600

    part2

--- JavaScript/Capitulo 2/javaScript.js
+++ JavaScript/Capitulo 2/javaScript.js
@@ -1,2 +1,12 @@
 fruta = ['platano','manzana','pera'];
-document.writeln(fruta[0]);
+document.writeln(fruta[0]+"<br>");
+
+numb = 1;
+while(numb <= 5){
+    document.writeln(numb+"<br>");
+    numb++;
+}
+
+for(let i=1; i<11; i++){
+    document.writeln("El numero es: "+i+"<br>")
+}
