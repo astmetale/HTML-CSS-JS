3face56d5cf9698d03fe399ae3841ee6457e9cab
astmetale <astmetale@gmail.com>
2026-02-24 23:57:25 -0600

    Strings

--- JavaScript/EjercicioCoflaCelulares/celular.js
+++ JavaScript/EjercicioCoflaCelulares/celular.js
@@ -1,10 +1,12 @@
 class Celular{
-    constructor(color, peso, rdp, rdc, ram){
+    constructor(marca, color, peso, rdp, rdc, ram, camaraLenta){
+        this.marca=marca;
         this.color=color;
         this.peso = peso;
         this.rdp=rdp;
         this.rdc=rdc;
         this.ram=ram;
+        this.camaraLenta=camaraLenta;
         this.encendido=false;
     }
     prender(){
@@ -24,18 +26,68 @@ class Celular{
         }
     }
     tomarFoto(){
-        alert(`Foto tomada en una resolucion de: ${this.rdc}`)
+        return `Foto tomada en una resolucion de: ${this.rdc}`;
     }
     grabarVideo(){
-        alert(`Video tomado en una resolucion de: ${this.rdc}`)
+        return `Video tomado en una resolucion de: ${this.rdc}`;
+    }
+    infoCelular(){
+        return `
+        <b> Marca: </b> ${this.marca} <br> 
+        <b> Color: </b> ${this.color} <br>
+        <b>  Peso: </b>  ${this.peso} <br>
+        <b> Tamaño de pantalla: </b> ${this.rdp} <br>
+        <b> Resolucion de camara: </b> ${this.rdc} <br>
+        <b> RAM: ${this.ram} </b> <br>
+        <b> Tiene Camara Lenta: </b> ${this.camaraLenta}  <br>
+        ${this.tomarFoto()} <br>
+        ${this.grabarVideo()} <br>
+        <br>
+        
+        `;
+
+    }
+}
+class GamaAlta extends Celular{
+    constructor(marca,color, peso, rdp, rdc, ram, camaraLenta, reconocimientoFacial, camaraExtra, camaraSuperLenta){
+        super(marca,color, peso, rdp, rdc, ram, camaraLenta);
+        this.reconocimientoFacial = reconocimientoFacial;
+        this.camaraExtra = camaraExtra;
+        this.camaraSuperLenta = camaraSuperLenta;
+    }
+    metReconocimientoFacial(){
+        return `Usando reconocimiento facial`;
+    }
+    metCamaraExtra(){
+        return `Usando la camara extra`;
+    }
+    metCamaraSuperLenta(){
+        return `Usando la camara super lenta`;
+    }
+    infoCelularGamaAlta(){
+        return `
+        
+        <b> Marca: </b> ${this.marca} <br> 
+        <b> Color: </b> ${this.color} <br>
+        <b>  Peso: </b>  ${this.peso} <br>
+        <b> Tamaño de pantalla: </b> ${this.rdp} <br>
+        <b> Resolucion de camara: </b> ${this.rdc} <br>
+        <b> RAM: </b> ${this.ram}  <br>
+        <b> Tiene Camara Lenta: </b> ${this.camaraLenta}  <br>
+        <b> Reconocimiento Facial </b>  ${this.reconocimientoFacial} <br>
+        <b> Camara Extra </b> ${this.camaraExtra} <br>
+        <b> Camara Super Lenta </b>  ${this.camaraSuperLenta}<br>
+        ${this.metReconocimientoFacial()} <br>
+        ${this.grabarVideo()} <br>
+        ${this.tomarFoto()} <br>
+        ${this.metCamaraExtra()} <br>
+        ${this.metCamaraSuperLenta()} <br>
+        <br>
+        `;
     }
 }
 
-celular1 = new Celular("rojo","150g","5 pulgadas","Full HD", "2GB");
-
-celular1.prender();
-celular1.tomarFoto();
-celular1.grabarVideo();
-celular1.reiniciar();
-celular1.prender();
-
+celular1 = new Celular("Sony","rojo","150g","5 pulgadas","Full HD", "2GB", "No tiene");
+celularGamaAlta = new GamaAlta("Huawei","Negro","130g","El tamaño es 4.2'","4K","8gb","Cuenta con camara lenta","Cuenta con reconocimiento facial","Si tiene camara extra","Cuenta con camara super lenta");
+document.writeln(celular1.infoCelular());
+document.writeln(celularGamaAlta.infoCelularGamaAlta());
---
+++ JavaScript/MetodosDeCadenas/cadena.js
@@ -0,0 +1,6 @@
+let cadena = "Cadena de prueba";
+let cadena2 = " y test 2";
+
+resultado = cadena.concat(cadena2);
+// Concat concatena dos strings
+document.writeln(resultado);
