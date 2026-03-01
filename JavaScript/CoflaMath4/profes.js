e9dbb58db3fdf16094b77104a19350883bc8a819
astmetale <astmetale@gmail.com>
2026-02-28 21:30:16 -0600

    Avance

--- JavaScript/CoflaMath4/profes.js
+++ JavaScript/CoflaMath4/profes.js
@@ -1,42 +1,53 @@
-
+const cantMaterias = 2;
+const profesores = 2;
 let materias = [];
-let op = 0;
-while(op != 5){
-    
-    op=parseInt(prompt(`1. Añadir las materias\n2. Registrar los profesores y alumnos de cada materia\n3. Ingresar materia y saber su profesor y alumnos`));
+let op;
+while(op!=4){
+    op = parseInt(prompt(`MENU DE OPCIONES\n1.- Registrar las 12 materias\n2.- Registrar profesores\n3.-Ingresar alumnos\n4.- Salir`));
     switch(op){
-    case 1:
-        alert(`A continuacion llenara las 12 materias`);
-        for(i=0; i<=11; i++){
-            materias[i]=prompt(`Ingrese el nombre de su materia numero ${(i+1)}`)
-        }
-        break;
-    case 2:
-            for(o=0;o<=11;o++){
-                materias[o]=[];
-                for(i=0;i<1;i++){
-                    let prof = prompt(`Para su materia ${materias[o]} indique el nombre de su profesor`);
-                    materias[o][i]=prof;
-                    for(j=1; i<=5; i++){
-                        let alum = prompt(`Ahora indique el alumno numero ${j} en la materia ${materias[i]}`);
-                        materias[o][j]=alum;
+        case 1:
+            if(materias.length!=2){
+                for(i=0;i<cantMaterias;i++){
+                    let materia = prompt(`Ingrese el nombre de su materia numero ${(i+1)}`);
+                    let profe = prompt(`Ingrese el nombre del profesor de la materia ${materia}`);
+                    materias[i] = {nombre: materia, profesor:profe, alumnos: null};
+                }
+            }else{
+                alert("Ya ha registrado sus materias previamente");
+            }
+                
+            break;
+        case 2:
+            let cadena = "";
+                if(materias.length===2){
+                    for(i=0;i<cantMaterias;i++){
+                        let name = materias[i];
+                        cadena+=(`El nombre de la materia es ${name.nombre} y su profesor es ${name.profesor}\n`);
                     }
+                    alert(`${cadena}`);
+                }else{
+                    alert("Primero debe usar la opcion 1");
+                }
+            break;
+        case 3:
+            if(materias.length===2){
+            let b = `Ingrese el nombre de su materia para ingresar los alumnos.\nLas materias registradas son:\n`;
+            for(i=0;i<2;i++){
+                b+=`Materia: ${materias[i].nombre} y profesor: ${materias[i].profesor}`;
+            }
+            let a = prompt(b);
+            for(i=0;i<2;i++){
+                if(materias[i].nombre == a){
+                    let temporal = "Los estudiantes son:\n";
+                    for(j=0;j<5;j++){
+                        temporal+=prompt(`Ingrese el nombre de su estudiante numero ${(j+1)}`);
+                    }
+                    materias[i].alumnos = temporal;
                 }
             }
-        break;
-    case 3:
-        let mat = prompt(`Ingrese la materia para saber su maestro y el nombre de todos los alumnos`);
-        for(i=0;i<=11;i++){
-            if(mat == materias[i]){
-                for(j=0;j<1;j++){
-                    alert(`Su maestro es ${materias[i][j]}`);
+            }else{
+                    alert("Primero debe usar la opcion 1");
                 }
-                let cadena = ``;
-                for(w=1;w<=5;w++){
-                    cadena+=`${materias[w]}`;
-                }
-            }
-        }
-        break;
+            break;
     }
 }
