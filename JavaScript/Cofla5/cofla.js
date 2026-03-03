e108c054417c9994e90c22aba9a2e5cca8d9afa5
astmetale <astmetale@gmail.com>
2026-03-02 20:56:21 -0600

    CoflaFinalizadoCapitulo6DOM

--- JavaScript/Cofla5/cofla.js
+++ JavaScript/Cofla5/cofla.js
@@ -6,20 +6,31 @@ const materias = {
     calculo:[91,6,3,"calculo"],
     programacion:[79,7,4,"programacion"],
     biologia:[75,9,2,"biologia"],
-    bbdd:[98,9,1,"bbdd"],
+    bbdd:[98,9,1,"BaseDeDatos"],
     algebra:[100,10,4,"algebra"]
 }
 
-const asistencia=()=>{
+const aprobo=()=>{
     for(materia in materias){
         let asistencias = materias[materia][0];
+        let promedio = materias[materia][1];
+        let trabajos = materias[materia][2];
+        console.log(materias[materia][3]);
         if(asistencias>=90){
-            console.log(materias[materia][3]);
-            console.log("%cAprobado","color:green");
+            console.log("%c   Asistencias en orden","color:green");
         }else{
-            console.log(materias[materia][3]);
-            console.log(`%cReprobado`,`color:red`);
+            console.log(`%c   Falta de asistencias`,`color:red`);
+        }
+        if(promedio >= 7){
+            console.log("%c   Promedio en orden","color:green");
+        }else{
+            console.log("%c   Promedio desaprobado","color:red");
+        }
+        if(trabajos >= 3){
+            console.log("%c   Trabajos practicos en orden","color:green");
+        }else{
+            console.log("%c   Faltan trabajos practicos","color:green");
         }
     }
 } 
-asistencia();
+aprobo();
