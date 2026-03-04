64c6bcbf66e275ae6bb6ac6acaa8a8eec32c9708
astmetale <astmetale@gmail.com>
2026-03-04 16:11:22 -0600

    Avance

---
+++ JavaScript/DOM/AtributosGlobales.js
@@ -0,0 +1,9 @@
+// contenteditable indica si el elemento puede ser modificable por el usuario (bool)
+let t = document.querySelector(".titulo");
+t.setAttribute("contentEditable","true");
+
+t = document.querySelector(".subtitulo");
+t.setAttribute("dir","rtl");
+
+t = document.querySelector(".oculto ");
+t.setAttribute("hidden","true");
