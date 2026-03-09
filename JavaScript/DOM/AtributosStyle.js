3c65e27645c87bc46897eb656ab22f9175a51e67
astmetale <astmetale@gmail.com>
2026-03-08 18:56:46 -0600

    MeQuedeEn 35Clases

---
+++ JavaScript/DOM/AtributosStyle.js
@@ -0,0 +1,3 @@
+const titulo = document.querySelector(`.titulo`);
+titulo.style.color="red";
+titulo.style.backgroundColor = "blue";
--- JavaScript/DOM/atributosInputs.js
+++ JavaScript/DOM/atributosInputs.js
@@ -1,9 +1,26 @@
-let input = document.querySelector(".input-normal");
-document.writeln(input.className+"<br><br>");
+// //class name
+// let input = document.querySelector(".input-normal1");
+// document.writeln(input.className+"<br><br>");
 
-document.writeln(input.value+"<br><br>");
+// //value
+// document.writeln(input.value+"<br><br>");
 
-document.writeln(input.type="range");
+// //type
+// document.writeln(input.type="range");
 
-let input2 = document.querySelector(".input-file");
-input2.accept="image/png";
+
+// //accept
+// let input2 = document.querySelector(".input-file");
+// input2.accept="image/png";
+
+// //minlength
+// const input = document.querySelector(".input-normal");
+// input.minLength = 4;
+
+// // placeholder
+// const input = document.querySelector(".input-normal");
+// input.placeholder = "Hola";
+
+// // required
+// const input = document.querySelector(".input-normal");
+// input.required = true;
