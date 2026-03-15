d813fa314958e09a02a7197cbe3dc505992cad8f
astmetale <astmetale@gmail.com>
2026-03-15 15:15:19 -0600

    Cofla1

--- JavaScriptMid/open/open.js
+++ JavaScriptMid/open/open.js
@@ -1,24 +1,2 @@
-// let youtube = "https://youtube.com";
-// let ventana = window.open(youtube);
-// ventana.close();
-// ventana.stop();
 
-// prompt("Dame un dato");
-// let respuesta = confirm("Deseas salir de la pagina?");
-// console.log(respuesta);
-
-// const screenLeft = window.screenLeft;
-// const screenTop = window.screenTop;
-// document.writeln(`El valor de screenLeft es: ${screenLeft} y el valor de screenTop es ${screenTop}`);
-
-// const scrolX = window.scrollX;
-// alert(scrolX);
-
-// const scroly = window.scrollY;
-// alert(scroly);
-let ventana = window.open("https://youtube.com");
-ventana.resizeBy(100,200);
-
-
-ventana.resizeTo(100,200);
-
+window.location.assign("https://youtube.com");
