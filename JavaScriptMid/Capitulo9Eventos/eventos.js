1e278be780b78c9cac7a54debe48f689d26d5ffb
astmetale <astmetale@gmail.com>
2026-03-17 22:11:33 -0600

    SigueEventosDelMouse

--- JavaScriptMid/Capitulo9Eventos/eventos.js
+++ JavaScriptMid/Capitulo9Eventos/eventos.js
@@ -1,5 +1,10 @@
 const button = document.querySelector(".button");
+const contenedor = document.querySelector(".contenedor");
 
-button.onclick = ()=>{
-    alert("Hola desde JS");
-}
+button.addEventListener("click",(e)=>{
+    alert("di click en el boton");
+    e.stopPropagation();
+});
+contenedor.addEventListener("click",(e)=>{
+    alert("di click en el contenedor");
+},true);
